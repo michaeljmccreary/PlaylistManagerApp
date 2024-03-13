0b61e2909eddc3c5093ed3b6c41033cd5ec35ce5
michaeljmccreary <>
2024-03-13 19:19:25 +0000

    Creating repository

---
+++ src/playlistmanager/PlayListManager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package playlistmanager;
+
+/**
+ *
+ * @author micha
+ */
+public class PlayListManager {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    
+}
